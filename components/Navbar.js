32edda4b75d269ff6c9faa551b706844d586e260
Chandru-Ghub <chandruinfo455@gmail.com>
2024-05-29 23:41:46 +0530

    added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,80 @@
 import { signOut } from "next-auth/react"
 import { useSession} from 'next-auth/react'
-import { Search, CloudSun , User, X, LogOut, SunMedium } from 'lucide-react';
-import { useState } from "react";
+import { Search, LocateFixed  , User, X, LogOut, SunMedium, MapPin } from 'lucide-react';
+import { useContext, useEffect, useState } from "react";
+import { myWeather } from "./HomePage";
+import axios from "axios";
+import CurrentGeoLocation from "./CurrentGeoLocation";
 export default function Navbar(){
     const { data: session } = useSession()
-    const [show,setShow] = useState(false)
+    const { position } = CurrentGeoLocation();
+    const [profile,setProfile] = useState(false)
+    const [show,setShow] = useState(true)
+    const [search, setSearch] = useState("");
+    const [getCity, setGetCity] = useState([]);
+    const [history, setHistory] = useState(false);
+    const [Weather, setWeather] = useContext(myWeather);
+  
+    // get cities from the open weather API
+    const searchCities = async () => {
+      setShow(true);
+        try {
+          const response = await axios.get(
+            `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=10&appid=ebd3f17fb8cc2328925eeec13a25afe9`);
+          let filteredDate = response.data.filter((item)=> item.name.toLowerCase().includes(search.toLowerCase()))
+          setGetCity(filteredDate);
+          console.log(filteredDate)
+        } catch (error) {
+          console.error(error);
+        }
+      
+    };
+  
+    // Debouncing Techinique
+    /// check from search input if exists call the function
+    useEffect(()=>{
+  
+      let timer = setTimeout(()=>{
+        if(search.trim()!== '')
+            searchCities()
+        else
+          setShow(false)
+  
+      },[400])
+  
+      //cleanup function
+      return ()=> clearTimeout(timer)
+    },[search])
+    
+    const resetWeather = () =>{
+        let geoLocation = {
+            lat: position.latitude,
+            lon: position.longitude,
+          };
+          setWeather(geoLocation);
+    }
+    // Get weather
+    const handleWeather = async (city) => {
+      setShow(false);
+      setSearch("");
+      let geoLocation = {
+        lat: city.lat,
+        lon: city.lon,
+      };
+  
+      // store searched data in mongoDB data base
+      setWeather(geoLocation);
+      let res = await axios.post(
+        "https://weatherappserver-w46g.onrender.com/data",
+        {
+          country_code: city.country,
+          lat: city.lat,
+          lon: city.lon,
+          city_name: city.name,
+        }
+      );
+      console.log(res.data);
+    };
     return(
         <div>
             <div className=" sticky top-0 flex py-2 bg-black justify-between items-center">
@@ -12,12 +82,32 @@ export default function Navbar(){
                 <SunMedium className="sun" size={50} color="white"/>
                 <h2 className="font-sans text-white p-1 text-2xl">Weathernow</h2>
                 </div>
-                <div className="flex justify-center items-center gap-2 rounded-full bg-neutral-900 px-5 py-2">
+                <div className="relative flex justify-center items-center gap-2 rounded-full bg-neutral-900 px-5 py-2">
                     <Search color="white" size={20}/>
-                    <input placeholder="Search city..." className="text-white outline-none bg-transparent w-80 h-8 border-none" type="text" />
+                    <input value={search} onChange={(e)=>setSearch(e.target.value)} placeholder="Search city..." className="text-white outline-none bg-transparent w-80 h-8 border-none" type="text" />
+                        {show&&<div className=" border-t-2 border-neutral-800 bg-neutral-900 text-neutral-400 absolute left-5 top-10 mt-2 right-5">
+                            {
+                                getCity.map((city)=>
+                                <div onClick={()=>handleWeather(city)} className="flex px-3 py-3 text-xs border-b-2 items-center gap-2 border-neutral-800 hover:bg-neutral-800 cursor-pointer">
+                                    <div>
+                                        <MapPin size={15}/>
+                                    </div>
+                                    <div>
+                                    <img className="w-5"
+                                    src={`https://flagsapi.com/${city.country}/flat/64.png`}
+                                    alt=""
+                                />
+                                    </div>
+                                    <div>({city.country}) - </div>
+                                    <div>{city.name}</div>
+                                    <div>{city.state}</div>
+                                </div>
+                                )
+                            }
+                        </div>}
                 </div>
-            {show &&<div className="absolute right-2 top-20 bg-neutral-900 text-white shadow-lg p-8 rounded flex flex-col gap-3">
-            <span onClick={()=>setShow(!show)} className="cursor-pointer absolute right-3 top-3 text-white"><X/> </span>                    <div>
+            {profile &&<div className="absolute right-2 top-20 bg-neutral-900 text-white shadow-lg p-8 rounded flex flex-col gap-3">
+            <span onClick={()=>setProfile(!profile)} className="cursor-pointer absolute right-3 top-3 text-white"><X/> </span>                    <div>
                         Name: <span>{session?.user?.name}</span>
                     </div>
                     <div>
@@ -25,10 +115,16 @@ export default function Navbar(){
                     </div>
                     <button onClick={() => signOut()} className=" w-fit mt-2 flex justify-start items-center bg-neutral-700 px-5 py-2"><LogOut/> Log out</button>
             </div>}
-            <div className=" px-4 py-2 mr-4 cursor-pointer flex gap-3 text-black justify-center items-center rounded-full bg-pink-200">
-            <User onClick={()=>setShow(!show)}/>
-            <span className="capitalize font-medium">{session?.user?.name}</span>
+           <div className="flex gap-3">
+           <div className="text-[14px] px-3 py-2 mr-4 cursor-pointer flex gap-3 text-black justify-center items-center rounded-full bg-pink-200 active:translate-y-1 hover:bg-pink-50 transition-all">
+            <LocateFixed onClick={()=>resetWeather()} /> Current Location
+           
             </div>
+            <div className="flex items-center justify-center w-10 h-10 cursor-pointer text-neutral-500 border-2 border-neutral-500 rounded-full p-1 mr-3">
+            <User size={23} onClick={()=>setProfile(!profile)} />
+            </div>
+           </div>
+            
         </div>
         </div>
     )
--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -1,12 +1,14 @@
 import { useContext, useEffect, useState } from "react";
 import { myWeather } from "./HomePage";
+import { Calendar, MapPin, Wind, Waves,  } from "lucide-react";
 import CurrentGeoLocation from "./CurrentGeoLocation";
 import axios from "axios";
 
-export default function WeatherDetails(){
+export default function WeatherDetails() {
   const [Weather, setWeather] = useContext(myWeather);
   const { position } = CurrentGeoLocation();
   const [data, setData] = useState([]);
+  const [hourly, setHourly] = useState([]);
   const [country, setCountry] = useState({});
 
   let CurrentDate = new Date();
@@ -35,14 +37,25 @@ export default function WeatherDetails(){
       lat = position.latitude;
       lon = position.longitude;
     }
-    
+
     if (lat && lon) {
-      console.log('lat',lat, 'lon',lon)
+      console.log("lat", lat, "lon", lon);
       try {
         let res2 = await axios.get(
           `https://api.weatherbit.io/v2.0/forecast/daily?days=8&lat=${lat}&lon=${lon}&key=bfe4846bdc59485c9708673ce7c360ea`
         );
-        console.log(res2)
+
+        let res1 = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=ebd3f17fb8cc2328925eeec13a25afe9&units=metric`)
+        const hourlyForeCast = []
+        const fiveDayForecast = res1.data.list.filter((item) => {
+              const filteredData = new Date(item.dt_txt).getDate()
+              if(date == filteredData || date+1 == filteredData){
+                return hourlyForeCast.push(filteredData)
+              }
+        })
+        setHourly(fiveDayForecast)
+        console.log(res2.data)
+        console.log(fiveDayForecast)
         setCountry(res2.data);
         setData(res2.data.data);
       } catch (error) {
@@ -56,42 +69,103 @@ export default function WeatherDetails(){
     getWeather();
   }, [position, Weather]);
 
-   return(
-   <div>
-        {data.length?<div className="bg-neutral-400 ">
-      <div className="current-weather">
-        <div className="city-location-data">
-          <p>
-            {country.city_name?country.city_name:'Location'}
-            <span className="material-symbols-outlined">location_on</span>
-          </p>
-          {data.length ? 
-            <div className="temp-datas">
-              <p>{data[0].temp} °c temp</p>
-              <p>{data[0].dewpt} °c dew</p>
-              <p>
-                {data[0].wind_spd} wind
-                <span className="material-symbols-outlined">air</span>
-              </p>
-              <div>
-                <img
-                  src={`https://cdn.weatherbit.io/static/img/icons/${data[0].weather.icon}.png`}
-                  alt=""
-                />
-                <p>{data[0].weather.description}</p>
-              </div>
+  return (
+    <div>
+      {data.length ? (
+        <div className="bg-red-400 p-5 flex gap-4 justify-center">
+          <div className="bg-black p-5 w-fit">
+            <div className=" text-white bg-stone-950 w-[330px] px-5 py-3 rounded-3xl ">
+              {data.length ? (
+                <div className="temp-datas">
+                  <span>Now</span>
+                  <div className="flex items-center justify-center gap-4">
+                    <div className="flex gap-3">
+                      <p className="text-5xl">{data[0].temp}</p>
+                      <span className="text-3xl">°c</span>
+                    </div>
+                    <div className="">
+                      <img
+                        className="w-20"
+                        src={`https://cdn.weatherbit.io/static/img/icons/${data[0].weather.icon}.png`}
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                  <p className="px-1 pb-3 border-b-2 border-neutral-800 mt-2 font-medium capitalize">
+                    {data[0].weather.description}
+                  </p>
+                  <div className="mt-2">
+                    <div className="flex gap-2 p-2">
+                      <p>
+                        <Calendar className="mr-1" />
+                      </p>
+                      <p className="text-neutral-400">{day}</p>
+                      <p className="text-neutral-400">{date},</p>
+                      <p className="text-neutral-400">{month}</p>
+                    </div>
+                    <div className="flex gap-2 p-2">
+                      <p>
+                        <MapPin className="mr-1" />
+                      </p>
+                      <p className="text-neutral-400">
+                        {country.city_name ? country.city_name : "Location"},
+                      </p>
+                      <p className="text-neutral-400">{country.country_code}</p>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
-          : ''}
-        </div>
-        <div className="country-location-time">
-          {/* <div className="geo-ctry">
+            <div>
+              <p className="w-fit my-2 text-white capitalize">7 Days forecast</p>
+            </div>
+
+            <div className="bg-stone-950 px-3 py-2 mt-4 rounded-3xl w-[330px] h-[350px] overflow-scroll no-scrollbar">
+              {
+                data.slice(1).map((list,i)=>
+                  <div className="px-3 py-1 flex mt-2 justify-between">
+                    <div className="flex items-center justify-center gap-2">
+                      <img className="w-12" src={`https://cdn.weatherbit.io/static/img/icons/${list.weather.icon}.png`} alt="" />
+                      <p className="text-white">{list.temp} °c </p>
+                    </div>
+                  <div className="flex gap-2 justify-center items-center mx-2">
+                  <p className="text-neutral-400">{new Date(
+                    list.datetime.split("-")[0],
+                    list.datetime.split("-")[1] - 1,
+                    list.datetime.split("-")[2]
+                  )
+                    .toString()
+                    .split(" ")[2]}</p>
+                    <p className="text-neutral-400">{new Date(
+                    list.datetime.split("-")[0],
+                    list.datetime.split("-")[1] - 1,
+                    list.datetime.split("-")[2]
+                  )
+                    .toString()
+                    .split(" ")[1]}</p>
+                  </div>
+                  <p className=" text-neutral-400 flex items-center justify-center">{new Date(
+                    list.datetime.split("-")[0],
+                    list.datetime.split("-")[1] - 1,
+                    list.datetime.split("-")[2]
+                  )
+                    .toString()
+                    .split(" ")[0]}</p>
+                  </div>
+                )
+              }
+            </div>
+            <div className="country-location-time">
+              {/* <div className="geo-ctry">
             <p>{country.country_code}</p>
             <img
               src={`https://flagsapi.com/${country.country_code}/flat/64.png`}
               alt=""
             />
           </div> */}
-          {/* <div className="dates-now">
+              {/* <div className="dates-now">
             <div className="DD">
               <p>{day}</p>
               <div>
@@ -104,9 +178,9 @@ export default function WeatherDetails(){
               <p>{time.split(':')[2].split(' ')[1]}</p>
             </div>
           </div> */}
-        </div>
-      </div>
-      {/* <div className="daily-forecast">
+            </div>
+          </div>
+          {/* <div className="daily-forecast">
       <div className="weather">
         {data&&data.slice(1).map((a, i) => (
           <div className="card1" key={i}>
@@ -139,11 +213,138 @@ export default function WeatherDetails(){
         ))}
       </div>
       </div> */}
-      </div>:<div className="progress">
-        {/* <img src={progress} alt="" /> */}
-        <div>Loading...</div>
-        </div>}
-      </div>
-      )
-  
+            <div className="text-white bg-stone-500 rounded-3xl p-5 w-[70%]">
+              <div className="bg-green-800 p-5 rounded-3xl ">
+                  <h2 className="font-bold">Today Highlights</h2>
+                  <div className="flex gap-4 mt-5 flex-wrap bg-yellow-800">
+                    <div className=" p-2  bg-red-700">
+                      <div className="flex bg-black justify-center items-center justify-between px-3 py-2">
+                        <div>Air Quality Index</div>
+                        <div className="bg-green-600 rounded-2xl px-3 py-1">Good</div>
+                      </div>
+                      <div className="flex gap-5 p-4 bg-yellow-600 items-center mt-3 rounded-2xl">
+                        <div>
+                          <Wind size={40}/>
+                        </div>
+                        <div className=" flex flex-col items-center justify-center">
+                          <p className=" text-neutral-400">PM25</p>
+                          <p className="text-3xl">3.90</p>
+                        </div>                     
+                        <div className=" flex flex-col items-center justify-center">
+                          <p className=" text-neutral-400">PM25</p>
+                          <p className="text-3xl">3.90</p>
+                        </div>                     
+                        <div className=" flex flex-col items-center justify-center">
+                          <p className=" text-neutral-400">PM25</p>
+                          <p className="text-3xl">3.90</p>
+                        </div>                     
+                        <div className=" flex flex-col items-center justify-center">
+                          <p className=" text-neutral-400">PM25</p>
+                          <p className="text-3xl">3.90</p>
+                        </div>                     
+                         </div>
+                      <div className="flex justify-between mt-5">
+                        <div className="bg-green-500 px-4 py-2 rounded-2xl w-[200px]">
+                          <p className="text-neutral-400">Humidity</p>
+                          <div className="flex justify-between mt-9">
+                            <div><Wind size={40}/></div>
+                            <div className="text-3xl">82%</div>
+                          </div>
+                        </div>
+                        <div className="bg-blue-500 px-4 py-2 rounded-2xl w-[200px]">
+                          <p className="text-neutral-400">Pressure</p>
+                          <div className="flex justify-between mt-9">
+                            <div><Wind size={40}/></div>
+                            <div className="text-3xl">82%</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="p-2  bg-red-700">
+                      <div className="flex bg-black justify-center items-center justify-between px-3 py-2">
+                        <div>Air Quality Index</div>
+                        <div className="bg-green-600 rounded-2xl px-3 py-1">Good</div>
+                      </div>
+                      <div className="flex gap-5 p-4 bg-yellow-600 items-center mt-3 rounded-2xl">
+                        <div>
+                          <Wind size={40}/>
+                        </div>
+                        <div className=" flex flex-col items-center justify-center">
+                          <p className=" text-neutral-400">PM25</p>
+                          <p className="text-3xl">3.90</p>
+                        </div>                     
+                        <div className=" flex flex-col items-center justify-center">
+                          <p className=" text-neutral-400">PM25</p>
+                          <p className="text-3xl">3.90</p>
+                        </div>                     
+                        <div className=" flex flex-col items-center justify-center">
+                          <p className=" text-neutral-400">PM25</p>
+                          <p className="text-3xl">3.90</p>
+                        </div>                     
+                        <div className=" flex flex-col items-center justify-center">
+                          <p className=" text-neutral-400">PM25</p>
+                          <p className="text-3xl">3.90</p>
+                        </div>                     
+                         </div>
+                      <div className="flex justify-between mt-5">
+                        <div className="bg-green-500 px-4 py-2 rounded-2xl w-[200px]">
+                          <p className="text-neutral-400">Humidity</p>
+                          <div className="flex justify-between mt-9">
+                            <div><Wind size={40}/></div>
+                            <div className="text-3xl">82%</div>
+                          </div>
+                        </div>
+                        <div className="bg-blue-500 px-4 py-2 rounded-2xl w-[200px]">
+                          <p className="text-neutral-400">Pressure</p>
+                          <div className="flex justify-between mt-9">
+                            <div><Wind size={40}/></div>
+                            <div className="text-3xl">82%</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div className="w-[100%]">
+                <div>Today at</div>
+                <div className=" w-[100%] no-scrollbar flex gap-5 bg-red-700 px-4 py-1 overflow-scroll">
+                  {
+                    hourly.map((item,i)=>
+                      <div className=" bg-stone-950 rounded-2xl flex gap-2 flex-col items-center justify-center py-4">
+                        <p>{item.dt_txt.slice(11,16) > 12 ? item.dt_txt.slice(11,16)+ ' PM' :item.dt_txt.slice(11,16)+' AM'} </p>
+                      <div className="flex gap-2">
+                      <p className="text-neutral-400 text-xs">{new Date(
+                    item.dt_txt.split(" ")[0]
+                  )
+                    .toString().slice(0,4)
+                    }</p>
+                      <p className="text-neutral-400 text-xs">{new Date(
+                    item.dt_txt.split(" ")[0]
+                  )
+                    .toString().slice(3,11).split(' ').reverse().join(' ')
+                    }</p>
+                      
+                      </div>
+                        <div className=" w-[120px] flex justify-center items-center">
+                          <img className="w-15 h-20"  src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`} alt="" />
+                        </div>
+                        <p>{item.main.temp}°</p>
+                      </div>
+                    )
+                  }
+                </div>
+              </div>
+              <div>
+
+              </div>
+            </div>
+        </div>
+      ) : (
+        <div className="progress">
+          {/* <img src={progress} alt="" /> */}
+          <div>Loading...</div>
+        </div>
+      )}
+    </div>
+  );
 }
